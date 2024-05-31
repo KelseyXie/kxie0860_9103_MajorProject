c4a2db6caa46465d587fd59f5a361c86212c39cb
KelseyXie <168155931+KelseyXie@users.noreply.github.com>
2024-05-31 22:38:57 +1000

    Updated gradientWave & building.
    1. Adjusted the size of the building to fit the MacBook screen size better.
    2. Added windows to the building.
    3. Adjusted the colours and layering of the GradientWave to enhance its subtlety.

--- baseCode/sketch.js
+++ baseCode/sketch.js
@@ -1,69 +1,99 @@
+//初始设置
+let gradientSky;
+let gradientSea
+const purpleWaves = [];
+const whiteWaves = [];
+
 function windowResized() {
   print("resized")
   resizeCanvas(windowWidth, windowHeight);
-  let skyXPos = -windowWidth/2;
-  let skyYPos = -windowHeight/2;
+  let skyXPos = -windowWidth / 2;
+  let skyYPos = -windowHeight / 2;
   let skyWidth = windowWidth;
-  let skyHeight = windowHeight/2+120
-  let amplitude = 50;
-  let yPercent1 = 0.2;
-  let yPercent2 = 0.5;
-  let color0 = color(30, 70, 140, 100); // Navy
-  let color1 = color(100, 150, 105); // Green
-  let color2 = color(230, 180, 50, 100); // Yellow
+  let skyHeight = windowHeight / 2 + 120
+  let amplitude1 = 50;
+  let amplitude2 = 20;
+  let yPercent1 = 0.25//0.6;
+  let yPercent2 = 0.5//0.9;
+  let yPercent3 = 0.1;
+  let yPercent4 = 0.4;
+  let color0 = color(20, 40, 120, 100); // Navy
+  let color1 = color(100, 150, 150, 100); // Green
+  let color2 = color(220, 180, 40, 100); // Yellow
   let color3 = color(160, 80, 50, 100); // Red
-  waves = [];
   for (let i = 0; i < 99; i++) {
-    waves.push(new WaveBrush(0, 148, width/2, 200));
+    waves.push(new WaveBrush(0, 148, width / 2, 200));
   }
-  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth+200, skyHeight, amplitude, yPercent1, yPercent2, color0, color1, color2, color3);
-  gradientSea = new GradientWave(skyXPos, 150, skyWidth+200, skyHeight, amplitude, yPercent1, yPercent2, color3, color2, color1, color0); 
+  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth + 200, skyHeight, amplitude1, yPercent1, yPercent2, color0, color1, color2, color3);
+  gradientSea = new GradientWave(skyXPos, 150, skyWidth + 200, skyHeight, amplitude2, yPercent3, yPercent4, color3, color2, color1, color0);
 }
-let gradientSky;
-let gradientSea
-let waves = [];
+
+//Set up.
 function setup() {
   angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight, WEBGL);
-  let skyXPos = -windowWidth/2;
-  let skyYPos = -windowHeight/2;
-  let skyWidth = windowWidth;
-  let skyHeight = windowHeight/2+120
-  let amplitude = 50;
-  let yPercent1 = 0.2;
-  let yPercent2 = 0.5;
-  let color0 = color(30, 70, 140, 100); // Navy
-  let color1 = color(100, 150, 105); // Green
-  let color2 = color(230, 180, 50, 100); // Yellow
-  let color3 = color(160, 80, 50, 100); // Red
-  water = new Water();
-  seagull1 = new Seagull(-100, -300, 300,100, 1, color(0));          // Original seagull
+  //创建水
+  water = new Water(60, 90);
+  //创建海鸥
+  seagull1 = new Seagull(-100, -300, 300, 100, 1, color(0));          // Original seagull
   seagull2 = new Seagull(-100, -100, 200, 100, 0.7, color(100)); // Smaller, lighter-colored seagull
-  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth+200, skyHeight, amplitude, yPercent1, yPercent2, color0, color1, color2, color3);
-  gradientSea = new GradientWave(skyXPos, 150, skyWidth+200, skyHeight, amplitude, yPercent1, yPercent2, color3, color2, color1, color0);
+  //创建背景
+  let skyXPos = -windowWidth / 2;
+  let skyYPos = -windowHeight / 2;
+  let skyWidth = windowWidth;
+  let skyHeight = windowHeight / 2 + 120
+  let amplitude1 = 50;
+  let amplitude2 = 20;
+  let yPercent1 = 0.25//0.6;
+  let yPercent2 = 0.5//0.9;
+  let yPercent3 = 0.1;
+  let yPercent4 = 0.4;
+  let color0 = color(20, 40, 120, 100); // Navy
+  let color1 = color(100, 150, 150, 100); // Green
+  let color2 = color(220, 180, 40, 100); // Yellow
+  let color3 = color(160, 80, 50, 100); // Red
+  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth + 200, skyHeight, amplitude1, yPercent1, yPercent2, color0, color1, color2, color3);
+  gradientSea = new GradientWave(skyXPos, 150, skyWidth + 200, skyHeight, amplitude2, yPercent3, yPercent4, color3, color2, color1, color0);
+  //创建建筑
   backgroundShadow = new BackgroundShadow(400, -120, 122);
   building = new Building(0, 120, 0, 0, 0);
-  for (let i = 0; i < 99; i++) {
-    waves.push(new WaveBrush(0, 148, width/2, 200));
+  //创建水波
+  for (let i = 0; i < 25; i++) {
+    purpleWaves.push(new WaveBrush(0, 170, width / 2, 200));
+  }
+  for (let i = 0; i < 10; i++) {
+    whiteWaves.push(new WaveBrush(-150, 250, width / 5, 200));
   }
 }
 
+//Draw
 function draw() {
   background("#FFFFFF")
+  //渐变背景
   gradientSky.display();
   gradientSea.display();
+  //建筑
   building.display();
-  building.reflection(20,20,20,120);
+  building.reflection(20, 20, 20, 120);
   backgroundShadow.display();
-  water.display();
+  //水
+  water.displayPerlinNoise();
+  //海鸥
   seagull1.move();
   seagull2.move();
   seagull1.display();
   seagull2.display();
-  for (let wave of waves) {
-    wave.edges();
-    wave.flock(waves, 1, 0, 1);
-    wave.update();
-    wave.display(1,1,55,0,255);
+  //水波
+  for (let purpleWave of purpleWaves) {
+    purpleWave.edges();
+    purpleWave.flock(purpleWaves, 1, 0, 1);
+    purpleWave.update(2, 1, 50, 50, 180);
+    purpleWave.display();
+  }
+  for (let whiteWave of whiteWaves) {
+    whiteWave.edges();
+    whiteWave.flock(whiteWaves, 1, 0, 1);
+    whiteWave.update(1, 1, 200, 200, 200);
+    whiteWave.display();
   }
 }
--- classes/gradientWave.js
+++ classes/gradientWave.js
@@ -1,5 +1,7 @@
+//This class creates a wave-like texture that has a gradient of 4 colour.
 class GradientWave {
   constructor(xPos, yPos, w, h, amplitude, yPercent1, yPercent2, color0, color1, color2, color3) {
+    // Set the X, Y position
     this.xPos = xPos; 
     this.yPos = yPos;
     // Width and height of the gradient background
@@ -26,24 +28,17 @@ class GradientWave {
     this.offset6 = random(100);
   }
 
+  //the display function draw the texture
   display() {
     push();
+    noStroke()
     translate(this.xPos, this.yPos);
-    noFill();
     let nx1 = this.offset1;
     let nx2 = this.offset2;
-    let nx3 = this.offset3;
-    let nx4 = this.offset4;
-    let nx5 = this.offset5;
-    let nx6 = this.offset6;
     // Draw the background from left to right
-    for (let x = -10; x <= this.w; x += 30) {
+    for (let x = -10; x <= this.w; x += 20) {
       let n1 = map(noise(nx1), 0, 1, -1, 1);
       let n2 = map(noise(nx2), 0, 1, -1, 1);
-      let n3 = map(noise(nx3), 0, 1, -1, 1);
-      let n4 = map(noise(nx4), 0, 1, -1, 1);
-      let n5 = map(noise(nx5), 0, 1, -1, 1);
-      let n6 = map(noise(nx6), 0, 1, -1, 1);
       // Add noise to the base lines to create a wave effect
       let waveHeight1 = this.yBase1 + this.amplitude * n1;
       let waveHeight2 = this.yBase2 + this.amplitude * n2;
@@ -52,44 +47,31 @@ class GradientWave {
       let gap2 = waveHeight2 - waveHeight1;
       let gap3 = this.h - waveHeight2;
       // Draw the first gradient section
-      for (let y = 0; y < gap1; y += 40) {
+      for (let y = 0; y < gap1; y += 15) {
         let inter = map(y, 0, gap1, 0, 1);
         let interColor = lerpColor(this.color0, this.color1, inter);
-        stroke(interColor);
-        strokeWeight(100);
-        // Use random deviation to create an irregular texture effect
-        ellipse(x + 10 * n3, y + 10 * n4, random(5, 30), random(5, 30)); 
+        fill(interColor);
+        circle(x, y ,100); 
       }
       // Draw the second gradient section
-      for (let y = 0; y < gap2; y += 40) {
+      for (let y = 0; y < gap2; y += 15) {
         let inter = map(y, 0, gap2, 0, 1);
         let interColor = lerpColor(this.color1, this.color2, inter);
-        stroke(interColor);
-        strokeWeight(100);
-        ellipse(x + 10 * n4, waveHeight1 + y + 10 * n5, random(5, 30), random(5, 30)); //random Deviation
+        fill(interColor);
+        circle(x , waveHeight1 + y , 100); 
       }
       // Draw the third gradient section
-      for (let y = 0; y < gap3; y += 40) {
+      for (let y = 0; y < gap3; y += 15) {
         let inter = map(y, 0, gap3, 0, 1);
         let interColor = lerpColor(this.color2, this.color3, inter);
-        stroke(interColor);
-        strokeWeight(100);
-        ellipse(x + 10 * n5, waveHeight2 + y + 10 * n6, random(5, 30), random(5, 30)); //random Deviation
+        fill(interColor);
+        circle(x , waveHeight2 + y , 100); 
       }
-
       nx1 += this.frequency;
       nx2 += this.frequency;
-      nx3 += this.frequency;
-      nx4 += this.frequency;
-      nx5 += this.frequency;
-      nx6 += this.frequency;
     }
     this.offset1 += 0.05;
     this.offset2 += 0.05;
-    this.offset3 += 0.05;
-    this.offset4 += 0.05;
-    this.offset5 += 0.05;
-    this.offset6 += 0.05;
     pop();
   }
 }
