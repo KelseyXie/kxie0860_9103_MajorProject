baaffd507d007d48955b5b195ce1687d8088f6df
KelseyXie <168155931+KelseyXie@users.noreply.github.com>
2024-06-01 16:39:52 +1000

    Updated waves.
    1. Use Perlin noise to create variation in the size of the waves.
    2. Change the colour of the waves to yellow during nighttime.

--- baseCode/sketch.js
+++ baseCode/sketch.js
@@ -1,179 +1,180 @@
 let gradientSky;
-let gradientSea
-const purpleWaves = [];
-const whiteWaves = [];
+let gradientSea;
+let purpleWaves = [];
+let whiteWaves = [];
 let sparks = []
 let illuminatedBuildings = [];
-
-function windowResized() {
-  print("resized")
-  resizeCanvas(windowWidth, windowHeight);
-  let skyXPos = -windowWidth / 2;
-  let skyYPos = -windowHeight / 2;
-  let skyWidth = windowWidth;
-  let skyHeight = windowHeight / 2 + 120
-  let amplitude1 = 50;
-  let amplitude2 = 20;
-  let yPercent1 = 0.25//0.6;
-  let yPercent2 = 0.5//0.9;
-  let yPercent3 = 0.1;
-  let yPercent4 = 0.4;
-  let color0 = color(20, 40, 120, 100); // Navy
-  let color1 = color(100, 150, 150, 100); // Green
-  let color2 = color(220, 180, 40, 100); // Yellow
-  let color3 = color(160, 80, 50, 100); // Red
-  for (let i = 0; i < 99; i++) {
-    waves.push(new WaveBrush(0, 148, width / 2, 200));
-  }
-  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth + 200, skyHeight, amplitude1, yPercent1, yPercent2, color0, color1, color2, color3);
-  gradientSea = new GradientWave(skyXPos, 150, skyWidth + 200, skyHeight, amplitude2, yPercent3, yPercent4, color3, color2, color1, color0);
-}
+let waveR
+let waveG
+let waveB
 
 //Set up.
 function setup() {
   angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight, WEBGL);
-  //创建水  // Create water
+  //Create water.
   water = new Water(60, 90);
-  //创建海鸥  // Create seagulls
-  seagull1 = new Seagull(-100, -300, 300, 100, 1, color(10));  //  bigger, dark-colored seagull
+  //Create seagulls.
+  seagull1 = new Seagull(-100, -300, 300, 100, 1, color(10));  // Bigger, dark-colored seagull
   seagull2 = new Seagull(-100, -100, 200, 100, 0.7, color(100)); // Smaller, lighter-colored seagull
   seagull3 = new Seagull(-600, -200, 200, 100, 0.5, color(30)); // Smallest, dark-colored seagull
-
-  // 创建背景 // Create gradient background
-  let skyXPos = -windowWidth / 2;
-  let skyYPos = -windowHeight / 2;
-  let skyWidth = windowWidth;
-  let skyHeight = windowHeight / 2 + 120
-  let amplitude1 = 50;
-  let amplitude2 = 20;
-  let yPercent1 = 0.25//0.6;
-  let yPercent2 = 0.5//0.9;
-  let yPercent3 = 0.1;
-  let yPercent4 = 0.4;
+  //Create gradient background.
   let color0 = color(20, 40, 120, 100); // Navy
   let color1 = color(100, 150, 150, 100); // Green
   let color2 = color(220, 180, 40, 100); // Yellow
   let color3 = color(160, 80, 50, 100); // Red
-  gradientSky = new GradientWave(skyXPos, skyYPos, skyWidth + 200, skyHeight, amplitude1, yPercent1, yPercent2, color0, color1, color2, color3);
-  gradientSea = new GradientWave(skyXPos, 150, skyWidth + 200, skyHeight, amplitude2, yPercent3, yPercent4, color3, color2, color1, color0);
-  //创建建筑  // Create building
+  gradientSky = new GradientWave(-windowWidth / 2, -windowHeight / 2, windowWidth + 200, windowHeight / 2 + 120, 50, 0.25, 0.5, color0, color1, color2, color3);
+  gradientSea = new GradientWave(-windowWidth / 2, 150, windowWidth + 200, windowHeight / 2 + 120, 20, 0.1, 0.4, color3, color2, color1, color0);
+  //Create buildinga and background building shadow.
   backgroundShadow = new BackgroundShadow(400, -120, 122);
   building = new Building(0, 120, 0, 0, 0);
-  //创建水波  // Create water waves
+  //Create purple and white water waves.
   for (let i = 0; i < 25; i++) {
-    purpleWaves.push(new WaveBrush(0, 170, width / 2, 200));
+    purpleWaves.push(new WaveBrush(0, 200, width / 2, 200));
   }
   for (let i = 0; i < 10; i++) {
-    whiteWaves.push(new WaveBrush(-150, 250, width / 5, 200));
+    whiteWaves.push(new WaveBrush(-150, 120, width / 4, 120));
   }
+  //  let colorPurple = color(70, 80, 180)
+  // let colorWhite = color(200, 200, 200)
+
+
 }
 
 //Draw
 function draw() {
   background("#FFFFFF")
-  //渐变背景  // Gradient background
+  //Draw gradient background.
   gradientSky.display();
   gradientSea.display();
-  //建筑  // Building
+  //Draw the building and its reflection.
   building.display();
   building.reflection(20, 20, 20, 120);
   backgroundShadow.display();
-  //水  // Water
+  //Draw water.
   water.displayPerlinNoise();
+  //Draw purple and white water waves.
 
-  //水波  // Water waves
+  //  let colorPurple = color(70, 80, 180)
+  // let colorWhite = color(200, 200, 200)
   for (let purpleWave of purpleWaves) {
     purpleWave.edges();
     purpleWave.flock(purpleWaves, 1, 0, 1);
-    purpleWave.update(2, 1, 50, 50, 180);
+    purpleWave.update(2, 1, waveR, waveG, waveB);
     purpleWave.display();
   }
   for (let whiteWave of whiteWaves) {
     whiteWave.edges();
     whiteWave.flock(whiteWaves, 1, 0, 1);
-    whiteWave.update(1, 1, 200, 200, 200);
+    whiteWave.update(1, 1, 180, 180, 220);
     whiteWave.display();
   }
 
-  //夜间蒙版 // NightMask class
-  let currentFrame = frameCount % (10 * 60);// Loop every 10 seconds
-  if (currentFrame < 2 * 60) {
-    sunRange = map(currentFrame, 0, 2 * 60, width / 2, 150)
-    //海鸥  // Seagulls
-    seagullAlpha = map(currentFrame, 0, 2 * 60, 250, 0)
-    seagull1.move();
-    seagull2.move();
-    seagull3.move();
-    seagull1.display(seagullAlpha);
-    seagull2.display(seagullAlpha)
-    seagull3.display(seagullAlpha)
-  } else if ((currentFrame < 8 * 60)) {
-    sunRange = 150
+  //Loop between day and night in 12-second intervals.
+  let currentFrame = frameCount % (12 * 60);
+  //The first 3 seconds of every interval.
+  if (currentFrame < 3 * 60) {
+    //Decrease the brightness area of NightMask.    
+    sunRange = map(currentFrame, 0, 3 * 60, width / 2, 200)
+    //Decrease the seagulls' opacity from 255 to 0.
+    seagullAlpha = map(currentFrame, 0, 3 * 60, 300, 0)
+    //The 3~9 seconds of every interval.
+  } else if ((currentFrame < 9 * 60)) {
+    sunRange = 200
+    //The last 3 seconds of every interval.
   } else {
-    sunRange = map(currentFrame, 8 * 60, 10 * 60, 150, width / 2)
-    seagullAlpha = map(currentFrame, 8 * 60, 10 * 60, 0, 250)
-    seagull1.move();
-    seagull2.move();
-    seagull3.move();
-    seagull1.display(seagullAlpha);
-    seagull2.display(seagullAlpha)
-    seagull3.display(seagullAlpha)
+    //Increase the brightness area of NightMask
+    sunRange = map(currentFrame, 9 * 60, 12 * 60, 200, width / 2)
+    //Increase the seagulls' opacity from 0 to 255.
+    seagullAlpha = map(currentFrame, 9 * 60, 12 * 60, 0, 300)
   }
-  let nightMask = new NightMask(-400, 100, width * 2, sunRange);
+  //Draw the nightMask.
+  let nightMask = new NightMask(-310, 100, width * 2, sunRange);
   nightMask.display();
+  //Draw the seagulls.
+  seagull1.move();
+  seagull2.move();
+  seagull3.move();
+  seagull1.display(seagullAlpha);
+  seagull2.display(seagullAlpha)
+  seagull3.display(seagullAlpha)
 
-  //初始建筑   // Initial building
+  //Draw initial building.
   building.display();
-  //天黑时亮灯  // Light up the building when it's dark
-  if (currentFrame > 3 * 60 && currentFrame < 7 * 60) {
 
-    // Create random fireworks based on time
+  //During nighttime,there will be:
+  //1- Fireworks
+  //2- Illuminated buildings
+  //3- lighted building's windows
+  //4- Yellow wave
+  if (currentFrame > 4 * 60 && currentFrame < 8 * 60) {
+    //Create random fireworks at random time.
     let fireworkX = random(-width / 3, width / 3)
     let fireworkY = random(-height / 2, -height / 5)
     let fireworkSize = random(1.5, 3)
     if (frameCount % 50 == 0 || frameCount % 140 == 0) {
       createFirework(fireworkX, fireworkY, fireworkSize)
-      // Create building illumination effects based on firework position
-
-      if (fireworkX < -310) {  //以建筑最高点为中线，左右两边产生不同的光照效果
+      //Create building illumination effects on the left or right side, 
+      //based on the firework position and the centerline of the building.
+      if (fireworkX < -310) {
         illuminatedBuildings.push(new Building(-10, 115, 230, 230, 200))
       }
       else {
         illuminatedBuildings.push(new Building(10, 115, 230, 230, 200))
       }
     }
-    // Display the lighting effects
+    //Draw the illumination effects of the building.
     for (let illuminatedBuilding of illuminatedBuildings) {
       illuminatedBuilding.display();
       illuminatedBuilding.illuminate();
       // Light up the building
-
       building.display();
-      
       building.lightup()
+
     }
-    // Play the Fireworks
+    //Change wave color to yellow.
+    waveR = 200
+    waveG = 170
+    waveB = 130
   }
+  else {
+    //Change wave color back to purple.
+    waveR = 100
+    waveG = 100
+    waveB = 170
+  }
+  // Draw the Fireworks
   for (let spark of sparks) {
     spark.show()
     spark.update()
   }
 }
 
-//个人功能部分 //Individual Work
-//夜间蒙版class // NightMask class
+function windowResized() {
+  print("resized")
+  resizeCanvas(windowWidth, windowHeight);
+  //Make sure the background fill the window.
+  let color0 = color(20, 40, 120, 100); // Navy
+  let color1 = color(100, 150, 150, 100); // Green
+  let color2 = color(220, 180, 40, 100); // Yellow
+  let color3 = color(160, 80, 50, 100); // Red
+  gradientSky = new GradientWave(-windowWidth / 2, -windowHeight / 2, windowWidth + 200, windowHeight / 2 + 120, 50, 0.25, 0.5, color0, color1, color2, color3);
+  gradientSea = new GradientWave(-windowWidth / 2, 150, windowWidth + 200, windowHeight / 2 + 120, 20, 0.1, 0.4, color3, color2, color1, color0);
+}
+
+// Individual Work：
+// NightMask class
 class NightMask {
   constructor(xPos, yPos, radius, sunRange) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.radius = radius;
-    this.gradientValue = 0.5;
+    this.gradientValue = 0.4;
     this.sunRange = sunRange
     this.space = 3;
     this.R = 10;
-    this.G = 30;
-    this.B = 80;
+    this.G = 20;
+    this.B = 60;
   }
   display() {
     noFill();
